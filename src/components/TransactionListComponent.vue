158ac073c7876a405724930b8be8b7e0aa403d01
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2022-12-22 10:27:35 +0700

    Menu transaksi hari ini

---
+++ src/components/TransactionListComponent.vue
@@ -0,0 +1,49 @@
+<script lang="ts">
+export default {
+  props: {
+    index: Number,
+    personName: String,
+    fishName: String,
+    isSold: Number,
+    value: Number,
+    price: Number,
+    date: String,
+  },
+};
+</script>
+
+<template>
+  <div
+    class="v-full h-max rounded-md bg-white/90 backdrop-md p-1 grid grid-cols-12"
+  >
+    <span
+      class="font-medium col-start-1 col-span-1 flex justify-center items-center"
+      ><p>{{ index }}</p></span
+    >
+    <span
+      class="font-medium col-start-2 col-span-2 flex justify-center items-center"
+      ><p>{{ personName }}</p></span
+    >
+    <span
+      class="font-medium col-start-4 col-span-2 flex justify-center items-center"
+      ><p>{{ fishName }}</p></span
+    >
+    <span
+      class="font-medium col-start-6 col-span-2 flex justify-center items-center"
+      ><p>{{ isSold ? "Penjualan" : "Pembelian" }}</p></span
+    >
+    <span
+      class="font-medium col-start-8 col-span-1 flex justify-center items-center"
+      ><p>{{ value }} Kg</p></span
+    >
+
+    <span
+      class="font-medium col-start-9 col-span-2 flex justify-center items-center"
+      ><p>{{ price }}</p></span
+    >
+    <span
+      class="font-medium col-start-11 col-span-2 flex justify-center items-center"
+      ><p>{{ date }}</p></span
+    >
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import TopoBackground from "@/components/TopoBackgroundComponent.vue";
 import FishList from "@/components/FishListComponent.vue";
+import TransactionList from "@/components/TransactionListComponent.vue";
 import axios from "axios";
 </script>
 
@@ -15,17 +16,35 @@ export default {
           stok: 0,
         },
       ],
+      transactionList: [
+        {
+          nama_orang: "",
+          nama_ikan: "",
+          penjualan: 0,
+          jumlah_kg: 0,
+          jumlah_harga: 0,
+          tanggal: "",
+        },
+      ],
     };
   },
   name: "HomeView",
   async mounted() {
     await this.getFishList();
+    await this.getDayTransactionList();
   },
   methods: {
     async getFishList() {
       await axios.get("http://localhost/api/fish-list.php").then((res) => {
         this.fishList = res.data;
       });
+    },
+    async getDayTransactionList() {
+      await axios
+        .get("http://localhost/api/day-transactions.php")
+        .then((res) => {
+          this.transactionList = res.data;
+        });
     },
   },
 };
@@ -35,7 +54,7 @@ export default {
   <TopoBackground />
   <div class="grid grid-cols-12 grid-rows-6 w-screen h-screen">
     <div
-      class="row-start-1 row-span-2 col-start-1 col-span-6 rounded-xl m-3 grid grid-cols-6 grid-rows-6"
+      class="row-start-1 row-span-2 col-start-1 col-span-5 rounded-xl m-3 grid grid-cols-6 grid-rows-6"
     >
       <div
         class="row-start-1 row-span-1 col-start-1 col-span-2 m-1 flex items-center justify-start rounded-md p-2 bg-white/30 backdrop-blur-sm"
@@ -55,12 +74,29 @@ export default {
       </div>
     </div>
     <div
-      class="row-start-1 row-span-2 col-start-7 col-span-6 bg-blue-900 rounded-xl m-3 grid grid-cols-6 grid-rows-6"
+      class="row-start-1 row-span-2 col-start-6 col-span-7 rounded-xl m-3 grid grid-cols-6 grid-rows-6"
     >
       <div
-        class="row-start-1 row-span-1 col-start-1 col-span-3 m-1 flex items-center justify-start rounded-md p-2"
+        class="row-start-1 row-span-1 col-start-1 col-span-2 m-1 flex items-center justify-start rounded-md p-2 bg-white/30 backdrop-blur-sm"
       >
-        <span class="font-medium text-white"><p>Transaksi Terakhir</p> </span>
+        <span class="font-bold text-black text-xl"
+          ><p>Transaksi Hari Ini</p></span
+        >
+      </div>
+      <div
+        class="row-start-2 row-span-5 col-start-1 col-span-6 rounded-md border-2 border-[#4d6bb9] scrollbar overflow-y-scroll p-1 flex flex-col space-y-0.5"
+      >
+        <span v-for="(transaction, index) in transactionList">
+          <TransactionList
+            :index="index + 1"
+            :person-name="transaction.nama_orang"
+            :fish-name="transaction.nama_ikan"
+            :is-sold="transaction.penjualan"
+            :value="transaction.jumlah_kg"
+            :price="transaction.jumlah_harga"
+            :date="transaction.tanggal"
+          />
+        </span>
       </div>
     </div>
     <div
